3bc29786c9d1f639b2f83f7aa5fb0203ff67b385
bin0905 <747084256@qq.com>
2018-11-07 09:56:08 +0800

    锤子官方网站”

---
+++ js/index.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Bin on 2018/11/5.
+ */
+var flag = false;
+$("header nav>ul").mouseleave(function () {
+    flag = false;
+});
+
+$("header nav>ul>li").eq(6).prevAll().hover(function () {
+    if (!flag) {
+        flag = true;
+        $(this).find(".layer").slideDown(600, "bounceOut");
+        $(this).find(".layer").show().end().siblings().find(".layer").hide();
+    } else if (flag) {
+        $(".layer").hide();
+        $(this).find(".layer").show();
+    }
+
+}, function () {
+    //$(this).find(".layer").slideUp();
+    $(".layer").slideUp(1000)
+
+});
+
+//var timer=null;
+var num = 0;
+$(".img li").eq(num).fadeIn(1500).siblings().fadeOut(1500);
+setInterval(function () {
+    num++;
+    if (num > 1) {
+        num = 0
+    }
+    $(".tit li").eq(num).addClass("active").siblings().removeClass("active");
+    $(".img li").eq(num).fadeIn(1500).siblings().fadeOut(1500);
+}, 4000);
+
+$(".tit li").click(function () {
+    $(this).addClass("active").siblings().removeClass("active");
+    $(".img li").eq($(this).index()).fadeIn(1500).siblings().fadeOut(1500);
+});
+
+$.ajax({
+    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/hotshop",
+//            url:"json.txt",
+    type: "get", /*get/post*/
+    data: {
+        //往后台传送数据   jsonp
+    },
+    async: false,
+    dataType: "json",
+    timeout: "10000",
+}).done(function (result) {
+    var data = result.data;
+    for (var i = 0; i < data.length; i++) {
+        $(".hot_shop").append(`
+           <li>
+                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+                    <div class="h_text">
+                        <h4>${data[i].name}</h4>
+                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+                    </div>
+                        <i class="${data[i].class}">${data[i].buy}</i>
+                    </li>
+         `).find("li").hover(function () {
+           var hovertit=$(this).find("h6").attr("data-hovertitle");
+            if(hovertit!=""){
+                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+            }
+
+        },function(){
+            var tit=$(this).find("h6").attr("data-title");
+            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+        });
+
+    }
+
+});
+$(".page_left").click(function(){
+    $(".hot_shop").css("left","0")
+});
+$(".page_right").click(function(){
+    $(".hot_shop").css("left","-1224px")
+});
