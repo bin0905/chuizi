19efe8b28d0da88d8513e9197946fee28b8cd9be
bin0905 <747084256@qq.com>
2018-11-11 20:51:53 +0800

    锤子官方网站

--- js/index.js
+++ js/index.js
@@ -38,193 +38,195 @@ $(".tit li").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
     $(".img li").eq($(this).index()).fadeIn(1500).siblings().fadeOut(1500);
 });
+
+
 /*
 
-$.ajax({
-    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/hotshop",
-//            url:"json.txt",
-    type: "get", /!*get/post*!/
-    data: {
-        //往后台传送数据   jsonp
-    },
-    async: false,
-    dataType: "json",
-    timeout: "10000",
-}).done(function (result) {
-    var data = result.data;
-    for (var i = 0; i < data.length; i++) {
-        $(".hot_shop").append(`
-           <li>
-                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
-                    <div class="h_text">
-                        <h4>${data[i].name}</h4>
-                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
-                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
-                    </div>
-                        <i class="${data[i].class}">${data[i].buy}</i>
-                    </li>
-         `).find("li").hover(function () {
-           var hovertit=$(this).find("h6").attr("data-hovertitle");
-            if(hovertit!=""){
-                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
-            }
+ $.ajax({
+ url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/hotshop",
+ //            url:"json.txt",
+ type: "get", /!*get/post*!/
+ data: {
+ //往后台传送数据   jsonp
+ },
+ async: false,
+ dataType: "json",
+ timeout: "10000",
+ }).done(function (result) {
+ var data = result.data;
+ for (var i = 0; i < data.length; i++) {
+ $(".hot_shop").append(`
+ <li>
+ <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+ <div class="h_text">
+ <h4>${data[i].name}</h4>
+ <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+ <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+ </div>
+ <i class="${data[i].class}">${data[i].buy}</i>
+ </li>
+ `).find("li").hover(function () {
+ var hovertit=$(this).find("h6").attr("data-hovertitle");
+ if(hovertit!=""){
+ $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+ }
 
-        },function(){
-            var tit=$(this).find("h6").attr("data-title");
-            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-        });
+ },function(){
+ var tit=$(this).find("h6").attr("data-title");
+ $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+ });
 
-    }
-});
-$.ajax({
-    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/newshop",
-//            url:"json.txt",
-    type: "get", /!*get/post*!/
-    data: {
-        //往后台传送数据   jsonp
-    },
-    async: false,
-    dataType: "json",
-    timeout: "10000",
-}).done(function (result) {
-    var data = result.data;
-    for (var i = 0; i < data.length; i++) {
-        $(".new_shop").append(`
-           <li>
-                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
-                    <div class="h_text">
-                        <h4>${data[i].name}</h4>
-                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
-                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
-                    </div>
-                        <i class="${data[i].class}">${data[i].buy}</i>
-                    </li>
-         `).find("li").hover(function () {
-            var hovertit=$(this).find("h6").attr("data-hovertitle");
-            if(hovertit!=""){
-                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
-            }
+ }
+ });
+ $.ajax({
+ url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/newshop",
+ //            url:"json.txt",
+ type: "get", /!*get/post*!/
+ data: {
+ //往后台传送数据   jsonp
+ },
+ async: false,
+ dataType: "json",
+ timeout: "10000",
+ }).done(function (result) {
+ var data = result.data;
+ for (var i = 0; i < data.length; i++) {
+ $(".new_shop").append(`
+ <li>
+ <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+ <div class="h_text">
+ <h4>${data[i].name}</h4>
+ <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+ <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+ </div>
+ <i class="${data[i].class}">${data[i].buy}</i>
+ </li>
+ `).find("li").hover(function () {
+ var hovertit=$(this).find("h6").attr("data-hovertitle");
+ if(hovertit!=""){
+ $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+ }
 
-        },function(){
-            var tit=$(this).find("h6").attr("data-title");
-            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-        });
+ },function(){
+ var tit=$(this).find("h6").attr("data-title");
+ $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+ });
 
-    }
-});
-$.ajax({
-    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/phoneshop",
-//            url:"json.txt",
-    type: "get", /!*get/post*!/
-    data: {
-        //往后台传送数据   jsonp
-    },
-    async: false,
-    dataType: "json",
-    timeout: "10000",
-}).done(function (result) {
-    var data = result.data;
-    for (var i = 0; i < data.length; i++) {
-        $(".phone_shop").append(`
-           <li>
-                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
-                    <div class="h_text">
-                        <h4>${data[i].name}</h4>
-                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
-                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
-                    </div>
-                        <i class="${data[i].class}">${data[i].buy}</i>
-                    </li>
-         `).find("li").hover(function () {
-            var hovertit=$(this).find("h6").attr("data-hovertitle");
-            if(hovertit!=""){
-                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
-            }
+ }
+ });
+ $.ajax({
+ url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/phoneshop",
+ //            url:"json.txt",
+ type: "get", /!*get/post*!/
+ data: {
+ //往后台传送数据   jsonp
+ },
+ async: false,
+ dataType: "json",
+ timeout: "10000",
+ }).done(function (result) {
+ var data = result.data;
+ for (var i = 0; i < data.length; i++) {
+ $(".phone_shop").append(`
+ <li>
+ <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+ <div class="h_text">
+ <h4>${data[i].name}</h4>
+ <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+ <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+ </div>
+ <i class="${data[i].class}">${data[i].buy}</i>
+ </li>
+ `).find("li").hover(function () {
+ var hovertit=$(this).find("h6").attr("data-hovertitle");
+ if(hovertit!=""){
+ $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+ }
 
-        },function(){
-            var tit=$(this).find("h6").attr("data-title");
-            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-        });
+ },function(){
+ var tit=$(this).find("h6").attr("data-title");
+ $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+ });
 
-    }
-});
-$.ajax({
-    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/guanfang",
-//            url:"json.txt",
-    type: "get", /!*get/post*!/
-    data: {
-        //往后台传送数据   jsonp
-    },
-    async: false,
-    dataType: "json",
-    timeout: "10000",
-}).done(function (result) {
-    var data = result.data;
-    for (var i = 0; i < data.length; i++) {
-        $(".guanfang_shop").append(`
-           <li>
-                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
-                    <div class="h_text">
-                        <h4>${data[i].name}</h4>
-                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
-                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
-                    </div>
-                        <i class="${data[i].class}">${data[i].buy}</i>
-                    </li>
-         `).find("li").hover(function () {
-            var hovertit=$(this).find("h6").attr("data-hovertitle");
-            if(hovertit!=""){
-                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
-            }
+ }
+ });
+ $.ajax({
+ url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/guanfang",
+ //            url:"json.txt",
+ type: "get", /!*get/post*!/
+ data: {
+ //往后台传送数据   jsonp
+ },
+ async: false,
+ dataType: "json",
+ timeout: "10000",
+ }).done(function (result) {
+ var data = result.data;
+ for (var i = 0; i < data.length; i++) {
+ $(".guanfang_shop").append(`
+ <li>
+ <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+ <div class="h_text">
+ <h4>${data[i].name}</h4>
+ <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+ <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+ </div>
+ <i class="${data[i].class}">${data[i].buy}</i>
+ </li>
+ `).find("li").hover(function () {
+ var hovertit=$(this).find("h6").attr("data-hovertitle");
+ if(hovertit!=""){
+ $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+ }
 
-        },function(){
-            var tit=$(this).find("h6").attr("data-title");
-            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-        });
+ },function(){
+ var tit=$(this).find("h6").attr("data-title");
+ $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+ });
 
-    }
-});
-$.ajax({
-    url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/zhoubian",
-//            url:"json.txt",
-    type: "get", /!*get/post*!/
-    data: {
-        //往后台传送数据   jsonp
-    },
-    async: false,
-    dataType: "json",
-    timeout: "10000",
-}).done(function (result) {
-    var data = result.data;
-    for (var i = 0; i < data.length; i++) {
-        $(".zhoubian_shop").append(`
-           <li>
-                    <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
-                    <div class="h_text">
-                        <h4>${data[i].name}</h4>
-                        <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
-                        <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
-                    </div>
-                        <i class="${data[i].class}">${data[i].buy}</i>
-                    </li>
-         `).find("li").hover(function () {
-            var hovertit=$(this).find("h6").attr("data-hovertitle");
-            if(hovertit!=""){
-                $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
-            }
+ }
+ });
+ $.ajax({
+ url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/zhoubian",
+ //            url:"json.txt",
+ type: "get", /!*get/post*!/
+ data: {
+ //往后台传送数据   jsonp
+ },
+ async: false,
+ dataType: "json",
+ timeout: "10000",
+ }).done(function (result) {
+ var data = result.data;
+ for (var i = 0; i < data.length; i++) {
+ $(".zhoubian_shop").append(`
+ <li>
+ <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
+ <div class="h_text">
+ <h4>${data[i].name}</h4>
+ <h6 data-hovertitle="${data[i].hovertitle}" data-title="${data[i].title}">${data[i].title}</h6>
+ <p class="price" >￥${data[i].price} <span>${data[i].oldprice}</span></p>
+ </div>
+ <i class="${data[i].class}">${data[i].buy}</i>
+ </li>
+ `).find("li").hover(function () {
+ var hovertit=$(this).find("h6").attr("data-hovertitle");
+ if(hovertit!=""){
+ $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+ }
 
-        },function(){
-            var tit=$(this).find("h6").attr("data-title");
-            $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-        });
+ },function(){
+ var tit=$(this).find("h6").attr("data-title");
+ $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
+ });
 
-    }
-});
-*/
-function ajaxNew(arr){
+ }
+ });
+ */
+function ajaxNew(arr) {
     for (var y = 0; y < arr.length; y++) {
         $.ajax({
-            url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/"+arr[y],
+            url: "https://www.easy-mock.com/mock/5be14891eeee54220716c03f/data/" + arr[y],
 //            url:"json.txt",
             type: "get", /*get/post*/
             data: {
@@ -236,7 +238,7 @@ function ajaxNew(arr){
         }).done(function (result) {
             var data = result.data;
             for (var i = 0; i < data.length; i++) {
-                $("."+arr[y]+"_shop").append(`
+                $("." + arr[y] + "_shop").append(`
            <li>
                     <div class="h_img"><img title="${data[i].name}" src="${data[i].images}" alt=""></div>
                     <div class="h_text">
@@ -247,22 +249,22 @@ function ajaxNew(arr){
                         <i class="${data[i].class}">${data[i].buy}</i>
                     </li>
          `).find("li").hover(function () {
-                    var hovertit=$(this).find("h6").attr("data-hovertitle");
-                    if(hovertit!=""){
-                        $(this).find("h6").addClass("red").get(0).innerHTML=hovertit;
+                    var hovertit = $(this).find("h6").attr("data-hovertitle");
+                    if (hovertit != "") {
+                        $(this).find("h6").addClass("red").get(0).innerHTML = hovertit;
                     }
                     $(this).find(".price").html("<button class='ckxq'>查看详情</button><button class='buycar'>加入购物车</button>")
-                    if($(this).find(".price").attr("data-button")==1){
+                    if ($(this).find(".price").attr("data-button") == 1) {
                         $(this).find(".price").find(".buycar").remove();
                     }
-                    if($(this).find(".price").attr("data-button")==2){
+                    if ($(this).find(".price").attr("data-button") == 2) {
                         $(this).find(".price").find(".buycar").addClass("none").html("已告罄");
                     }
-                },function(){
-                    var tit=$(this).find("h6").attr("data-title");
+                }, function () {
+                    var tit = $(this).find("h6").attr("data-title");
                     //var aPrice=$(this).find(".price").attr("data-aprice");
-                    $(this).find("h6").removeClass("red").get(0).innerHTML=tit;
-                    $(this).find(".price").get(0).innerHTML=$(this).find(".price").attr("data-aprice");
+                    $(this).find("h6").removeClass("red").get(0).innerHTML = tit;
+                    $(this).find(".price").get(0).innerHTML = $(this).find(".price").attr("data-aprice");
                 });
 
             }
@@ -270,13 +272,13 @@ function ajaxNew(arr){
 
     }
 }
-$(".page_left").click(function(){
-    $(".hot_shop").css("left","0")
+$(".page_left").click(function () {
+    $(".hot_shop").css("left", "0")
 });
-$(".page_right").click(function(){
-    $(".hot_shop").css("left","-1224px")
+$(".page_right").click(function () {
+    $(".hot_shop").css("left", "-1224px")
 });
-var arrName=["hot","new","phone","guanfang","peijian","zhoubian","jingxuan"]
+var arrName = ["hot", "new", "phone", "guanfang", "peijian", "zhoubian", "jingxuan"]
 ajaxNew(arrName);
 
 
